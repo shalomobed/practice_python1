ea073d610b29b7c9a6811b57075526f2f398dd9d
shalomobed <127734332+shalomobed@users.noreply.github.com>
2024-03-14 07:46:24 -0400

    Create salary.py

---
+++ salary.py
@@ -0,0 +1,5 @@
+wage = int(input("Please enter your hourly wage: "))
+hours = int(input("Please enter the number of hours worked per week: "))
+weeks = int(input("Please enter the number of weeks worked this year: "))
+annual_salary = str(wage*hours*weeks)
+print ("Your salary so far this year is $" + annual_salary)
